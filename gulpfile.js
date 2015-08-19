f9ee225356b51594fca73a8d1287db281536cf58
Thomas Warlop <thomas.warlop@gmail.com>
2015-08-19 17:40:55 +0200

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,40 +16,38 @@ var gulp = require('gulp'),
     glob = require('glob');
 
 
-
 gulp.task('default', ['admin-theme', 'theme-page-styles']);
 
-gulp.task('admin-theme', function()
-{
+gulp.task('admin-theme', function () {
     return gulp.src([
-            'resources/assets/js/admin/config.js',
-            'resources/assets/js/admin/services.js',
-            'resources/assets/js/admin/directives.js',
-            'resources/assets/js/admin/translations.js',
-            'resources/assets/js/admin/controllers.js',
-        ])
+        'resources/assets/js/admin/config.js',
+        'resources/assets/js/admin/models.js',
+        'resources/assets/js/admin/translations.js',
+        'resources/assets/js/admin/services/*.js',
+        'resources/assets/js/admin/directives/*.js',
+        'resources/assets/js/admin/controllers/*.js',
+    ])
         .pipe(plumber())
         .pipe(concat('unify.js'))
-        .pipe(rename(function(path){
+        .pipe(rename(function (path) {
             path.extname = '.min.js';
         }))
         //do not mangle, or function names etc will fail.
-        .pipe(uglify({mangle:false}))
+        .pipe(uglify({mangle: false}))
         .pipe(gulp.dest('resources/assets/js/admin'));
 });
 
 //global startup compiler
-gulp.task('theme-page-styles', function()
-{
+gulp.task('theme-page-styles', function () {
     gulp.src('resources/assets/less/pages/**/*.less')
         .pipe(plumber())
         .pipe(less())
-        .pipe(rename(function(path){
+        .pipe(rename(function (path) {
             path.extname = '.css';
         }))
         .pipe(gulp.dest('resources/assets/css/pages/'))
         .pipe(minify())
-        .pipe(rename(function(path){
+        .pipe(rename(function (path) {
             path.extname = '.min.css';
         }))
         .pipe(gulp.dest('resources/assets/css/pages/'));
