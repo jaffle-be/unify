3c5b4087b645165167d3a7381bc65c0b51681618
Thomas Warlop <thomas.warlop@gmail.com>
2015-08-19 18:12:17 +0200

    fix social links bug

--- resources/views/blog/elements/author.blade.php
+++ resources/views/blog/elements/author.blade.php
@@ -7,11 +7,13 @@
         <div class="overflow-h">
 
             <h4>{{ $post->user->name }}</h4>
-            <ul class="list-inline">
-                @foreach($post->user->socialLinks->available() as $name => $url)
-                    <li><a target="_blank" href="{{ $url }}"><i class="fa fa-{{ $name }}"></i></a></li>
-                @endforeach
-            </ul>
+            @if($post->user->socialLinks)
+                <ul class="list-inline">
+                    @foreach($post->user->socialLinks->available() as $name => $url)
+                        <li><a target="_blank" href="{{ $url }}"><i class="fa fa-{{ $name }}"></i></a></li>
+                    @endforeach
+                </ul>
+            @endif
         </div>
         <p>{{$post->user->bio}}</p>
 
