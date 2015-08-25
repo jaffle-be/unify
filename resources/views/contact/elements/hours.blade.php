6cff3463ff72180227ab7c40a745452d8c02c8a1
Thomas Warlop <thomas.warlop@gmail.com>
2015-08-25 15:34:13 +0200

    remove duplication of opening hours

--- resources/views/contact/elements/hours.blade.php
+++ resources/views/contact/elements/hours.blade.php
@@ -1,9 +1,5 @@
 <!-- Business Hours -->
 <div class="headline"><h2>{{ Theme::setting('contactHoursTitle') }}</h2></div>
 <ul class="list-unstyled margin-bottom-30">
-    <? $teller = 0 ?>
-    @while($teller < 3)
-        <li>{{ $contact->hours }}</li>
-        <? $teller++ ?>
-    @endwhile
+    <li>{{ $contact->hours }}</li>
 </ul>
