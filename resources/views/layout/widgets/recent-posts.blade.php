b97a780a572b12259f70e87747a8d9814707b06b
Thomas Warlop <thomas.warlop@gmail.com>
2015-09-04 12:37:23 +0200

    fix home recent posts widget

--- resources/views/layout/widgets/recent-posts.blade.php
+++ resources/views/layout/widgets/recent-posts.blade.php
@@ -2,20 +2,28 @@
     <div class="headline-center-v2 headline-center-v2-dark margin-bottom-60">
         <h2>{{ Theme::setting('widgetsRecentPostsTitle') }}</h2>
         <span class="bordered-icon"><i class="fa fa-th-large"></i></span>
+
         <p>{{ Theme::setting('widgetsRecentPostsIntro') }}</p>
-    </div><!--/Headline Center V2-->
+    </div>
+    <!--/Headline Center V2-->
 
     <div class="row">
         @foreach($latest as $post)
             <div class="col-sm-4">
                 <div class="thumbnails-v1">
                     <div class="thumbnail-img">
-                        <img class="img-responsive" src="{{ asset($post->thumbnail(460)) }}" alt="">
+                        <a href="{{ route('store.blog.show', [$post]) }}">
+                            <img class="img-responsive" src="{{ asset($post->thumbnail(460)) }}" alt="">
+                        </a>
                     </div>
                     <div class="caption">
                         <h3><a href="{{ route('store.blog.show', [$post]) }}">{{ $post->title }}</a></h3>
+
                         <p>{{ $post->extract }}</p>
-                        <p><a class="read-more" href="#">{{ Lang::get('Unify::front.read-more') }}</a></p>
+
+                        <p>
+                            <a class="read-more" href="{{ route('store.blog.show', [$post]) }}">{{ Lang::get('Unify::front.read-more') }}</a>
+                        </p>
                     </div>
                 </div>
             </div>
